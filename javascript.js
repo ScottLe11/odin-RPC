96e54961c5bb91b6aa0eaaa85d5dfdf3cafa22a2
Scott <scottle220044@gmail.com>
2025-09-08 00:44:50 -0700

    Implemented basic rules of RPS

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 var humanScore = 0;
-var compScore = 0;
+var computerScore = 0;
 
 //console.log("Hello World");
 
@@ -22,8 +22,34 @@ function getComputerChoice(){
 function getHumanChoice(){
     var choice = prompt("Choose rock, paper, or scissors");
     return choice;
-    
 }
 
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice){
+    var human = humanChoice.toLowerCase();
+    var computer = computerChoice.toLowerCase();
+
+    if (human == computer){
+        console.log("Tied!");
+    }
+
+    else if ((human == "rock" && computer == "scissors") ||
+        (human == "paper" && computer == "rock") ||
+        (human == "scissors" && computer == "paper")
+        ){
+            console.log("You win. " + human + " beats " + computer); 
+        ++humanScore;
+    }
+
+    else{
+        console.log("You lose. " + computer + " beats " + human);
+        ++computerScore;
+    }
+    
+}
+
+var humanSelection = getHumanChoice();
+var computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
