03294b83850321360c014a6d58fa26ea0d2a79a1
Scott <scottle220044@gmail.com>
2025-09-08 00:28:48 -0700

    Implemented and tested computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+//randomize computer choice between RPS
+function getComputerChoice(){
+    var random = Math.random();
+    if (random >= .66){
+        return "rock";
+    }
+    else if (random >= .33){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+//function get
